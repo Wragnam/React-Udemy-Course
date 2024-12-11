69a7ae42006f795489283af04d830a5c073a66d9
Patrick <claassenspatrick1@gmail.com>
2024-12-11 14:20:50 +0200

    Lecture 66

--- 01-starting-project/src/components/CoreConcepts/CoreConcepts.jsx
+++ 01-starting-project/src/components/CoreConcepts/CoreConcepts.jsx
@@ -1,13 +1,14 @@
 import CoreConcept from "./CoreConcept.jsx";
 import { CORE_CONCEPTS } from "../../data-with-examples.js";
+import Section from "../Section.jsx";
 
 export default function CoreConcepts() {
-  return (<section id="core-concepts">
+  return (<Section id="core-concepts">
     <h2>Core Concepts</h2>
     <ul>
       {CORE_CONCEPTS.map((conceptItem) => (
         <CoreConcept key={conceptItem.title} {...conceptItem} />
       ))}
     </ul>
-  </section>);
+  </Section>);
 }
