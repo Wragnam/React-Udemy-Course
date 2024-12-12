ed45762c1ea12e6df5567ec08df58d4b56bc3e33
Patrick <claassenspatrick1@gmail.com>
2024-12-12 09:08:11 +0200

    Lecture 86

--- 07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -1,11 +1,10 @@
 export default function Log({ turns }) {
- 
   return (<ol id="log">
     {turns.map(({square, player}) => {
-        const block = "{" + square.row + "," + square.col + "}";
+        const block = `{${square.row}, ${square.col}}`;
         
         return(<li key={block}>
-            Player {player} played: {block}
+            {player} selected: {block}
         </li>)
     })}
   </ol>);
