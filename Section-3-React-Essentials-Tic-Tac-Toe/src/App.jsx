08839be9f048ff81b6a2add69325ac3712733b82
Patrick <claassenspatrick1@gmail.com>
2024-12-12 16:01:17 +0200

    Renamed import

--- Section-3-React-Essentials-Tic-Tac-Toe/src/App.jsx
+++ Section-3-React-Essentials-Tic-Tac-Toe/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
-import Player from "./Components/Player";
+import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
