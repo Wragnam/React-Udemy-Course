e5c5b43a586ce249cba4b4bafa3cce52c6f597d7
Patrick <claassenspatrick1@gmail.com>
2024-12-12 09:21:37 +0200

    Lecture 89

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,8 @@ import GameBoard from "./components/GameBoard";
 import Player from "./Components/Player";
 import Log from "./components/Log";
 
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
