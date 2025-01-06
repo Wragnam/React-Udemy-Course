f8877aadbf5cd8e6ef502f71323306f15bdf2e91
Patrick <claassenspatrick1@gmail.com>
2025-01-06 11:28:46 +0200

    Lecture 194: Deriving values, outputting questions

--- Section-12-Practice-Project-Quiz-App/src/components/Quiz.jsx
+++ Section-12-Practice-Project-Quiz-App/src/components/Quiz.jsx
@@ -1,8 +1,25 @@
 import { useState } from "react";
+import QUESTIONS from "../questions";
 
 export default function Quiz() {
-  const [activeQuestion, setActiveQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Question</p>;
+  const activeQuestion= QUESTIONS[userAnswers.length];
+
+  function handleSelectAnswer(selectedAnswer){
+    setUserAnswers(prevAnswers=>[...prevAnswers, selectedAnswer]);
+  }
+
+  return (
+    <div id="quiz">
+    <div id="question">
+      <h2>{activeQuestion.text}</h2>
+      <ul id="answers">
+        {activeQuestion.answers.map(answer => <li key={answer} className="answer">
+          <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+        </li>)}
+      </ul>
+    </div>
+    </div>
+  );
 }
