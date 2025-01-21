7c1b3fe6ac88e787c8324d20527bcdd7fc705d0c
Patrick <claassenspatrick1@gmail.com>
2025-01-21 13:47:46 +0200

    417: Challenge! Own implementation of getting event details and deleting the event using useMutation and useQuery

--- Section-25-Tanstack-Query-Handeling-HTTP-Requests-With-Ease/src/components/Events/EventDetails.jsx
+++ Section-25-Tanstack-Query-Handeling-HTTP-Requests-With-Ease/src/components/Events/EventDetails.jsx
@@ -1,8 +1,39 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { fetchEvent, deleteEvent, queryClient } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const id = params.id;
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["eventInfo", { id: id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const {
+    mutate,
+    isPending: isPendingDelete,
+    isError: isErrorDelete,
+    error: errorDelete,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id: id });
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +42,52 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <LoadingIndicator />}
+      {isError && (
+        <ErrorBlock
+          title="Failed to load event details"
+          message={
+            error.info?.message ||
+            "Failed to load event details, please try again later."
+          }
+        />
+      )}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            {!isPendingDelete && (
+              <nav>
+                <button onClick={handleDelete}>Delete</button>
+                <Link to="edit">Edit</Link>
+              </nav>
+            )}
+          </header>
+
+          {isPendingDelete && <LoadingIndicator />}
+          {isErrorDelete && (
+            <ErrorBlock
+              title="Failed to delete event"
+              message={
+                errorDelete.info?.message ||
+                "Failed to delete event, please try again later."
+              }
+            />
+          )}
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date} @ {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
