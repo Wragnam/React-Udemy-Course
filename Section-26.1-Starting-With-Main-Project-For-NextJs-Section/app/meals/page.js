72969d0662a1fd19584eae10bbdb6c116df237c2
Patrick <claassenspatrick1@gmail.com>
2025-01-22 13:36:34 +0200

    447: Outputting meals data & images with unknown dimension using the 'fill' prop

--- Section-26.1-Starting-With-Main-Project-For-NextJs-Section/app/meals/page.js
+++ Section-26.1-Starting-With-Main-Project-For-NextJs-Section/app/meals/page.js
@@ -1,15 +1,25 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="/meals/p1">Meal 1</Link>
-      </p>
-      <p>
-        <Link href="/meals/p2">Meal 2</Link>
-      </p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
