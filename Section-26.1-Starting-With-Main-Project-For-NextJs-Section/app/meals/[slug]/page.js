df9f4e12ac06b685ad9da2e1552d8dec0e7b6f57
Patrick <claassenspatrick1@gmail.com>
2025-01-22 15:40:22 +0200

    454: Loading & rendering meal details via dynamic routes & route parameters

--- Section-26.1-Starting-With-Main-Project-For-NextJs-Section/app/meals/[slug]/page.js
+++ Section-26.1-Starting-With-Main-Project-For-NextJs-Section/app/meals/[slug]/page.js
@@ -1,13 +1,35 @@
 import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
 export default async function MealPage({ params }) {
-  const p = await params;
+  const meal = getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Meal Page {p.slug} </h1>
-      <p>
-        <Link href="/meals">Back</Link>
-      </p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
