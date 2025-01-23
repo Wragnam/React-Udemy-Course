e24c6908e403e3ef5b4aa611f107df63bd370199
Patrick <claassenspatrick1@gmail.com>
2025-01-23 16:08:34 +0200

    494: Preparing paths with getStaticPaths & working with fallback pages

--- Section-26.3-NextJS-Page-Router-Main-Project/.next/static/chunks/webpack.js
+++ Section-26.3-NextJS-Page-Router-Main-Project/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b24ace671e9546b"; }
+/******/ 		__webpack_require__.h = function() { return "96291845cae790fe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
