cdeb1d5acee6cff507a3537eeea43cd076065200
Patrick <claassenspatrick1@gmail.com>
2025-01-23 14:14:15 +0200

    488: Adding custom components & css modules

--- Section-26.3-NextJS-Page-Router-Main-Project/.next/static/chunks/webpack.js
+++ Section-26.3-NextJS-Page-Router-Main-Project/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e10c9a1205ce985f"; }
+/******/ 		__webpack_require__.h = function() { return "3e2793da40e5fd62"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ Section-26.3-NextJS-Page-Router-Main-Project/pages/[meetup]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://www.studying-in-uk.org/wp-content/uploads/2019/05/study-in-london.jpg"
+      title="First meetup in London"
+      address="Some street in London, London"
+      description="The meetup description"
+    />
+  );
+}
