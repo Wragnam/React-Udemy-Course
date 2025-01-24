008416a4dc4a8e23b0cde6f37a68182c5f709397
Patrick <claassenspatrick1@gmail.com>
2025-01-24 09:15:07 +0200

    499: Getting data from the database

--- Section-26.3-NextJS-Page-Router-Main-Project/.next/static/chunks/webpack.js
+++ Section-26.3-NextJS-Page-Router-Main-Project/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "64d6ee9191b83ee9"; }
+/******/ 		__webpack_require__.h = function() { return "1d3fbe2726b15013"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Section-26.3-NextJS-Page-Router-Main-Project/pages/index.js
+++ Section-26.3-NextJS-Page-Router-Main-Project/pages/index.js
@@ -1,25 +1,6 @@
 import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "First Meetup",
-    image:
-      "https://www.studying-in-uk.org/wp-content/uploads/2019/05/study-in-london.jpg",
-    address: "London Ave, London",
-    description: "This is a first meetup",
-  },
-  {
-    id: "m2",
-    title: "Second Meetup",
-    image:
-      "https://www.studying-in-uk.org/wp-content/uploads/2019/05/study-in-london.jpg",
-    address: "London Ave, London",
-    description: "This is a second meetup",
-  },
-];
-
 export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
