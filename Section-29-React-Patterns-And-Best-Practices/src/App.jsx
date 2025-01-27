4af23878d0b6f4b5ca26480fe575c526fd90ef1c
Patrick <claassenspatrick1@gmail.com>
2025-01-27 14:24:17 +0200

    540: Introducing compound components (components that work together and does not have too much functionality individually)

--- Section-29-React-Patterns-And-Best-Practices/src/App.jsx
+++ Section-29-React-Patterns-And-Best-Practices/src/App.jsx
@@ -1,5 +1,40 @@
+import Accordian from "./components/Accordion/Accordion";
+import AccordianItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordian className="accordion">
+          <AccordianItem
+            className="accordion-item"
+            title="We got 20 years of experience"
+          >
+            <article>
+              <p>You can&apos;t go wrong with us.</p>
+              <p>
+                We are in the business of planning highly individualized
+                vacation trips for more than 20 years
+              </p>
+            </article>
+          </AccordianItem>
+          <AccordianItem
+            className="accordion-item"
+            title="We're working with local guides"
+          >
+            <article>
+              <p>We are not doing this alone from our office.</p>
+              <p>
+                Instead we are working with local guides to ensure a safe and
+                pleasant vacation
+              </p>
+            </article>
+          </AccordianItem>
+        </Accordian>
+      </section>
+    </main>
+  );
 }
 
 export default App;
