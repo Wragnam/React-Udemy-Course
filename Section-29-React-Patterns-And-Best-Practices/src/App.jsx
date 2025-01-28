8b13f7da7d1aa8f8a7e68be5714d6988da7961e2
Patrick <claassenspatrick1@gmail.com>
2025-01-28 08:50:42 +0200

    548: Handeling keys dynamically

--- Section-29-React-Patterns-And-Best-Practices/src/App.jsx
+++ Section-29-React-Patterns-And-Best-Practices/src/App.jsx
@@ -78,10 +78,13 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES}>
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
           {(item) => <Place item={item} />}
         </SearchableList>
-        <SearchableList items={["item 1", "item 2", "item 3"]}>
+        <SearchableList
+          items={["item 1", "item 2", "item 3"]}
+          itemKeyFn={(item) => item}
+        >
           {(item) => item}
         </SearchableList>
       </section>
