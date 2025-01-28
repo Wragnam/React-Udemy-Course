bd32702952373aef8c0d417a2fae30bbe2d6ab57
Patrick <claassenspatrick1@gmail.com>
2025-01-28 13:56:35 +0200

    581: Exploring the base types

---
+++ Section-32-React-With-Typescript/basics.ts
@@ -0,0 +1,16 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+let age: number;
+
+age = 21;
+
+let userName: string;
+
+userName = "Patrick";
+
+let isInstructor: boolean;
+
+isInstructor = true;
