e53eac8ae53302b30d73243122e18222914ae03b
Patrick <claassenspatrick1@gmail.com>
2025-01-28 12:14:55 +0200

    572: Grouping tests together with suites

--- Section-31-Testing-React-Apps-Unit-Testing/src/components/Greeting.test.js
+++ Section-31-Testing-React-Apps-Unit-Testing/src/components/Greeting.test.js
@@ -1,15 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("Renders Hello World! as text", () => {
-  // Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("Renders Hello World! as text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText(/hello world/i);
+    // Assert
+    const helloWorldElement = screen.getByText(/hello world/i);
 
-  expect(helloWorldElement).toBeInTheDocument();
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
