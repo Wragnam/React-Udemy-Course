50218fd6a1ca8cfd946a5c8e3b59b5a42ccab97b
Patrick <claassenspatrick1@gmail.com>
2025-01-29 16:21:06 +0200

    617: Working with state

--- Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/NewPost.jsx
+++ Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/NewPost.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [text, setText] = useState("");
+
   function changeBodyHandler(event) {
-    console.log(event.target.value);
+    setText(event.target.value);
   }
 
   return (
@@ -11,6 +14,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{text}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
