75fc8d98496acc9db346ef4e6f50a8323b99465d
Patrick <claassenspatrick1@gmail.com>
2025-01-29 16:04:46 +0200

    615: Preparing the app for state management

--- Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/PostsList.jsx
+++ Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/PostsList.jsx
@@ -1,11 +1,15 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 export default function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post name="Piet" text="Text property!" />
-      <Post name="Patrick" text="Hi" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post name="Piet" text="Text property!" />
+        <Post name="Patrick" text="Hi" />
+      </ul>
+    </>
   );
 }
