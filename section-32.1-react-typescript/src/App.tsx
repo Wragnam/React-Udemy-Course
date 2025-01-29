79b02438fcfa4f4bb4dd4508dce2dbcf1fb38bb6
Patrick <claassenspatrick1@gmail.com>
2025-01-29 13:00:06 +0200

    597: Managing state & typescript

--- section-32.1-react-typescript/src/App.tsx
+++ section-32.1-react-typescript/src/App.tsx
@@ -4,9 +4,12 @@ import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((oldTodo) => [...oldTodo, newTodo]);
+  };
 
   return (
     <div>
