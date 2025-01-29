ecf8fba7045ed7b0b14ce43de0502b3b350f20bc
Patrick <claassenspatrick1@gmail.com>
2025-01-29 16:25:36 +0200

    618: Lifting state up

--- Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/NewPost.jsx
+++ Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/NewPost.jsx
@@ -1,23 +1,15 @@
-import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [text, setText] = useState("");
-
-  function changeBodyHandler(event) {
-    setText(event.target.value);
-  }
-
+function NewPost({ onBodyChange, onAuthorChange }) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={onBodyChange} />
       </p>
-      <p>{text}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={onAuthorChange}/>
       </p>
     </form>
   );
--- Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/PostsList.jsx
+++ Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/PostsList.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 export default function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [authorName, setAuthorName] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setAuthorName(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post name="Piet" text="Text property!" />
+        <Post name={authorName} text={enteredBody} />
         <Post name="Patrick" text="Hi" />
       </ul>
     </>
