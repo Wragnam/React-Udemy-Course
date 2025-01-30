19ec13560186d7e8333bf21849dac823b4c77f28
Patrick <claassenspatrick1@gmail.com>
2025-01-30 13:13:33 +0200

    660: Splitting components into multiple components

--- Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/ExpenseItem.js
+++ Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem({ expense }) {
-  const month = expense.date.toLocaleString("en-US", { month: "long" });
-  const year = expense.date.toLocaleString("en-US", { day: "2-digit" });
-  const day = expense.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
