a298a32ea67c246406568c17f2f33b445fd3ea36
Patrick <claassenspatrick1@gmail.com>
2025-01-30 12:22:56 +0200

    656: Outputting dynamic values

--- Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/ExpenseItem.js
+++ Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseDate = new Date(2025, 1, 30);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 134.87;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$100.98</div>
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
