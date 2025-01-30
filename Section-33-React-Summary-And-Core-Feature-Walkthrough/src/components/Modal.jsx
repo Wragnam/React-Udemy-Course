d2dd0aca60723398aadb010298aad8042d647150
Patrick <claassenspatrick1@gmail.com>
2025-01-30 10:49:03 +0200

    634: Linking & navigating

--- Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/Modal.jsx
+++ Section-33-React-Summary-And-Core-Feature-Walkthrough/src/components/Modal.jsx
@@ -1,9 +1,15 @@
 import classes from "./Modal.module.css";
+import { useNavigate } from "react-router-dom";
 
-export default function Modal({ children, onClose }) {
+export default function Modal({ children }) {
+  const navigate = useNavigate();
+  function closeHandler() {
+    navigate("..");
+  }
+
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose} />
+      <div className={classes.backdrop} onClick={closeHandler} />
       <dialog className={classes.modal} open>
         {children}
       </dialog>
