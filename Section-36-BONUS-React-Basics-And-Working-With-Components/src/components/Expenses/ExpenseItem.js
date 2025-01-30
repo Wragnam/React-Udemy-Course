8d6463f5a5829f765eae79b8e32eeb445e0c5f15
Patrick <claassenspatrick1@gmail.com>
2025-01-30 14:01:39 +0200

    664: Organizing component files

--- Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/Expenses/ExpenseItem.js
+++ Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,5 @@
-import Card from "./UI/Card";
-import ExpenseDate from "./Expenses/ExpenseDate";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem({ expense }) {
