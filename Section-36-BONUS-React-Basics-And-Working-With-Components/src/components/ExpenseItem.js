f98710000bcb6737660c07c5c7cae64e2e5a7ac5
Patrick <claassenspatrick1@gmail.com>
2025-01-30 12:57:29 +0200

    659: Adding "normal" js logic to components

--- Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/ExpenseItem.js
+++ Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 export default function ExpenseItem({ expense }) {
+  const month = expense.date.toLocaleString("en-US", { month: "long" });
+  const year = expense.date.toLocaleString("en-US", { day: "2-digit" });
+  const day = expense.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expense.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
