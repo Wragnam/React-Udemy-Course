4508ddbd9552e59aaf947549ef491d88908a2d6d
Patrick <claassenspatrick1@gmail.com>
2025-01-31 08:55:06 +0200

    676: Listening to events & working with event handlers

--- Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/Expenses/ExpenseItem.js
+++ Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,20 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem({ expense }) {
+  let title = expense.title;
+
+  function clickHandler() {
+    title = "Updated";
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
