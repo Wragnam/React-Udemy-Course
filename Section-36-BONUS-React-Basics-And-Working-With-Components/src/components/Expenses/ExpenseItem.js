09212176e0d5febc1827630ef95d9859e251cf0a
Patrick <claassenspatrick1@gmail.com>
2025-01-31 10:46:52 +0200

    692: Cleaning up project

--- Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/Expenses/ExpenseItem.js
+++ Section-36-BONUS-React-Basics-And-Working-With-Components/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,15 @@
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-import { useState } from "react";
 
 export default function ExpenseItem({ expense }) {
-  const [title, setTitle] = useState(expense.title);
-
-  function clickHandler() {
-    setTitle("Updated!");
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
